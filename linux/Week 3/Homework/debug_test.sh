0920f7763c7014a583c0ff3cbc5e66def7e93ca7
tehtw <tehtw@protonmail.com>
2020-11-05 17:06:09 +0800

    First

--- linux/Week 3/Homework/debug_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIVISOR=${1:-0}
-echo $DIVISOR
-expr 12 / $DIVISOR
--- linux/Week 3/Homework/loop_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-counter=0
-while [ $counter -lt 10 ]
-do
-echo $counter
-counter=`expr $counter + 1`
-done
